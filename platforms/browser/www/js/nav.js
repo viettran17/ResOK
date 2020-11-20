b42820b4e3bfe89856b443c39c8ae3aa222aa61f
viettran17 <viettqgch170138@fpt.edu.vn>
2020-11-20 22:28:10 +0700

    Final

--- platforms/browser/www/js/nav.js
+++ platforms/browser/www/js/nav.js
@@ -11,10 +11,10 @@ function home() {
                                 <img src = "${results[i].Restaurant_Image}" style="max-width: 500px; width: 100%; height: auto;">
                                 <h1 class="card-title">${results[i].Restaurant_Name}</h1>
                             </a>
-                            <p class="card-text">Restaurant Type: ${results[i].Restaurant_Type}</p>
-                            <p>Average Rating: <span>${parseFloat((Number(results[i].Food_Rate) + Number(results[i].Clean_Rate) + Number(results[i].Service_Rate))/3).toFixed(1)}</span></p>
+                            <h5 class="card-text">${results[i].Restaurant_Type}</h5>
+                            <p>Average Rating: <span>${parseFloat((Number(results[i].Food_Rate) + Number(results[i].Clean_Rate) + Number(results[i].Service_Rate))/3).toFixed(1)}</span><i class="fa fa-star fa_custom"></i></p>
                             <p class="card-text">Address: ${results[i].Restaurant_Address}</p>
-                            <button id="DeleteRes" rateId="${results[i].id}" class="btn btn-outline-primary">Delete</button>
+                            <button id="DeleteRes" rateId="${results[i].id}" class="btn btn-outline-primary">Delete <span><i class="fa fa-trash" ></i></span></button>
                         </div>
                     </div>
                 </div>
@@ -71,17 +71,24 @@ $(document).ready(function() {
             <div class="modal-dialog" role="document">
                 <div class="">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">${Restaurant_Detail.Restaurant_Name}</h5>
+                    <h1 class="modal-title" id="exampleModalLabel">${Restaurant_Detail.Restaurant_Name}</h1>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <h4 class="card-title">${Restaurant_Detail.Restaurant_Name}</h4>
-                    <p class="card-text">${Restaurant_Detail.Restaurant_Type}</p>
-                    <p>Average Rating: <span>${parseFloat((Number(results[i].rate_food) + Number(results[i].rate_clean) + Number(results[i].rate_service))/3).toFixed(1)}</span></p>
+                    <img src = "${Restaurant_Detail.Restaurant_Image}" style="max-width: 500px; width: 100%; height: auto;">
+                    <h3 class="card-text">${Restaurant_Detail.Restaurant_Type}</h3>
+                    <p class="card-text">Address: ${Restaurant_Detail.Restaurant_Address}</p>
+                    <p class="card-text">Food Rate   : ${Restaurant_Detail.Food_Rate}<i class="fa fa-star fa_custom"></i></p>
+                    <p class="card-text">Clean Rate  : ${Restaurant_Detail.Clean_Rate}<i class="fa fa-star fa_custom"></i></p>
+                    <p class="card-text">Service Rate: ${Restaurant_Detail.Service_Rate}<i class="fa fa-star fa_custom"></i></p>
+                    <h4>Average Rating: <span>${parseFloat((Number(Restaurant_Detail.Food_Rate) + Number(Restaurant_Detail.Clean_Rate) + Number(Restaurant_Detail.Service_Rate))/3).toFixed(1)}</span><i class="fa fa-star fa_custom"></i></h4>
+                    <h4 class="card-text">Average Price: ${Restaurant_Detail.Price}<i class="fa fa-usd" aria-hidden="true"></i></h4>
+                    <p class="card-text">Note: ${Restaurant_Detail.Notes}</p>
                 </div>
                 <div class="modal-footer">
+                    <p class="card-text">Rate time:${Restaurant_Detail.Date}</p>
                     <button type="submit" class="btn btn-secondary" data-dismiss="modal" href="#homepage">Close</button>
                 </div>
                 </div>
